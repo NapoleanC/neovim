3968d6997bfcd5f6e2d8068f2fa4aff77dd5e150
Christopher Gholmieh <ccchris.side@gmail.com>
2023-12-29 00:15:23 +0000

    LSP keymaps.

--- lua/neovim-plugins/neovim-lspconfig.lua
+++ lua/neovim-plugins/neovim-lspconfig.lua
@@ -15,16 +15,22 @@ local Module = {
 	config = function()
 		local LSPConfig = require("lspconfig")
 		local LSPs = {
-			"lua_ls"
+			"lua_ls",
+			"clangd",
 		}
 
 		local Capabilities = require("cmp_nvim_lsp").default_capabilities()
 		for _, LSP in ipairs(LSPs) do
 			LSPConfig[LSP].setup({
-				capabilities = Capabilities
+				capabilities = Capabilities,
 			})
 		end
-	end
+
+		vim.keymap.set("n", "<C-i>", ":lua vim.lsp.buf.implementation()<CR>", { silent = true })
+		vim.keymap.set("n", "<C-r>", ":lua vim.lsp.buf.references()<CR>", { silent = true })
+		vim.keymap.set("n", "<C-a>", ":lua vim.lsp.buf.code_action()<CR>", { silent = true })
+		vim.keymap.set("n", "<C-h>", ":lua vim.lsp.buf.hover()<CR>", { silent = true })
+	end,
 }
 
 --< Functions:
